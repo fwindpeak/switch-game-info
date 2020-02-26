ccadd92c3ef4068d0cfc84e2586da715fa4f0207
fwindpeak <fwindpeak@gmail.com>
2020-02-26 11:12:12 +0800

    add download-img.py

---
+++ download-img.py
@@ -0,0 +1,14 @@
+from ConstData import AreaCode
+import json
+import os
+
+if not os.path.exists("./images"):
+    os.mkdir("./images")
+
+for item in AreaCode:
+    sales_data = json.load(open('sales-%s-%s.json'%(item[0],item[1])))
+    os.chdir('./images')
+    for content in sales_data['contents']:
+        img_url = content['hero_banner_url']
+        os.system('wget '+img_url)
+    os.chdir('..')
